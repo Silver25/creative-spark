c9a89cbf91ea796ad4a7ddebc4e7f3da5fc7381b
Silver25 <silvio.martinec.ie@gmail.com>
2025-03-21 20:03:33 +0000

    Create new urls.py file inside the 'Home' app and reuse-adopt code from Project's urls.py

---
+++ home/urls.py
@@ -0,0 +1,20 @@
+"""
+URL configuration for the Home app.
+
+This module defines the URL patterns for the application. 
+It includes the routes for the main home page.
+
+Routes:
+    - Home Page: Accessible at the root URL ('/'), handled by `views.index` view.
+
+Modules:
+    - django.urls: Contains tools to define URL patterns.
+    - views: Contains the view functions for the app.
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
--- home/views.py
+++ home/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 
 def index(request):
     """
-    docstring / description of the function:
-    A simple view to return the index page
+    Renders the index.html template.
+
+    This view serves as the entry point for the application, rendering
+    the main landing page (index.html). It doesn't process any specific
+    data or context, but can be extended in the future to include
+    dynamic content.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response with the rendered index.html template.
     """
     return render(request, 'home/index.html')
