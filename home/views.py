5afe243837ed149a93e8390eeef903561387785d
Silver25 <silvio.martinec.ie@gmail.com>
2025-03-21 19:52:41 +0000

    Create view to render index.html file

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    docstring / description of the function:
+    A simple view to return the index page
+    """
+    return render(request, 'home/index.html')
