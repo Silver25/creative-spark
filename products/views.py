8a398f0bc2b427d644a00b8ca7cf28acb047506b
Silver25 <silvio.martinec.ie@gmail.com>
2025-04-22 14:42:17 +0100

    Update Products view for search queries

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
-# Create your views here.
 
 def all_products(request):
     """
@@ -9,24 +10,37 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    query = None
+
+    # request for the search form
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-     """
-     docstring / description of the function:
-     A view to display details about one product
-     """
- 
-     product = get_object_or_404(Product, pk=product_id)
- 
-     context = {
-         'product': product,
-     }
- 
-     return render(request, 'products/product_detail.html', context)
+    """
+    docstring / description of the function:
+    A view to display details about one product
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
