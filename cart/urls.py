5d2526eb4aa4d40d3ed6a73e3de617b5e94460a6
Silver25 <silvio.martinec.ie@gmail.com>
2025-04-25 19:00:29 +0100

    Create 'cart' urls.py with path

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,6 @@
-# from django.contrib import admin - removed because didn't used
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
+    path('', views.view_cart, name='view_cart'),
 ]
