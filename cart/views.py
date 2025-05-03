00f80a98cde9b934f1e0868c6135af9d7d0d9f27
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-03 18:45:08 +0100

    Update 'add_to_bag' view with import and messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib import messages
 from products.models import Product
 
 # Create your views here.
@@ -13,14 +14,18 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag!')
 
     request.session['cart'] = cart
 
