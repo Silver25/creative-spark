2ca7dd628c1c4b16ec5f5631fa3a432a493bbfa2
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-06 19:40:37 +0100

    Add Stripe public and private key codes

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RLpIhCyGRl4qoSjxoCY5BupZEGIFvzmuc3nnb8gZiCFTmEaXHT1GpuMm85GVEjOMxE04kiQP11U0DAef6TV2i7V00vDGrVPg3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
