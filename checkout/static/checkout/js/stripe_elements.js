9afc299106df4cb54ce82e4badce432fe98ec5d3
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-10 19:02:07 +0100

    Add trigger in case of error request 400

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -113,5 +113,8 @@ form.addEventListener('submit', function(ev) {
                 }
             }
         });
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
+        location.reload();
     })
 });
