364f23fdb3c46a41e6aaad01251b191439a9ebcd
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-15 11:04:07 +0100

    Create files for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn creative-bliss.wsgi:application
--- boutique/settings.py
+++ boutique/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+import dj_database_url
+from pathlib import Path
 if os.path.isfile('env.py'):
     import env
 
@@ -23,14 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Allows error messages to be displayed in the process of dev and testing.
 # CommandError: If DEBUG is False you must set 'settings.ALLOWED_HOSTS'.
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'creative-bliss-7f2ee6904dd8.herokuapp.com',
+]
 
 
 # Application definition
@@ -121,12 +126,17 @@ WSGI_APPLICATION = 'boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
