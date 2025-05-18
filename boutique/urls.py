c8445b0dcb6ce91bc784ded0089283d2336fb1af
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-18 20:16:31 +0100

    Add URL path to new app within project URLs

--- boutique/urls.py
+++ boutique/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
+from .views import faq_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +28,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('faq/', faq_list, name='faq'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'boutique.views.handler404'
