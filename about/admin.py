32578a9a64c91c7eb6dcfad87eafb0f7f9b688db
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-19 04:36:33 +0100

    Create admin fields

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import About
 
-# Register your models here.
+
+class AboutAdmin(admin.ModelAdmin):
+    list_display = (
+        'label_1',
+        'paragraph_1',
+        'label_2',
+        'paragraph_2',
+    )
+
+
+admin.site.register(About, AboutAdmin)
