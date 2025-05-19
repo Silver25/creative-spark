44f5e9a45c477c3648f2fddea8cc8678e661b92f
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-19 09:58:26 +0100

    Update of 'about' app

--- about/views.py
+++ about/views.py
@@ -5,4 +5,4 @@ from .models import About
 def about_view(request):
     about = About.objects.all()
 
-    return render(request, "about/about.html", {'about': about})
+    return render(request, "about/about.html", {"about": about})
